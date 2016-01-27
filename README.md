af5906d0350efb26e3e4ccd6a846af5d4d98079f
Scott Meves <scott@quikly.com>
2016-01-27 03:32:17 -0500

    updated dyno naming conventions

--- README.md
+++ README.md
@@ -9,15 +9,22 @@ Set up jobs for when your want to scale your app.
 
 For example if you create a job with the task:
 
-    ruby scaler.rb -a quikly-prod -p web -q 3 -c 15 -s PX
+    ruby scaler.rb -a quikly-prod -p web -q 3 -c 15 -s Performance-L
 
 ... and set the next run to 5:00 UTC. The number of web dynos for the app "my-app" will be scaled to 3 at 5:00 in the morning UTC.
 
 ```
 EST/EDT -> +4 -> UTC
-1AM ET --> 5  UTC:   ruby scaler.rb -a quikly-prod -p web -q 1 -s 1X
-8AM ET --> 12 UTC:   ruby scaler.rb -a quikly-prod -p web -q 2 -s PX
+1AM ET --> 5  UTC:   ruby scaler.rb -a quikly-prod -p web -q 1 -s Standard-1
+8AM ET --> 12 UTC:   ruby scaler.rb -a quikly-prod -p web -q 2 -s Performance-L
 ```
+
+Common scenarios:
+
+    ruby scaler.rb -a quikly-prod -p web -q 2 -c 4 -s Standard-2X
+    ruby scaler.rb -a quikly-prod -p web -q 2 -c 15 -s Performance-L
+
+
 
 A note from Heroku:
 
--- scaler.rb
+++ scaler.rb
@@ -11,7 +11,7 @@ OptionParser.new do |opts|
   opts.on("-a", "--app NAME", 'Your heroku app name') { |v| options[:app] = v }
   opts.on("-p", "--process WEB_OR_WORKER", 'web or worker') { |v| options[:process] = v }
   opts.on("-q", "--quantity NUM_DYNOS", 'Number of dynos you want to scale to') { |v| options[:quantity] = v }
-  opts.on("-s", "--size DYNO_SIZE", '1X, 2X or PX') { |v| options[:size] = v.upcase }
+  opts.on("-s", "--size DYNO_SIZE", '1X, 2X or PX') { |v| options[:size] = v.downcase }
   opts.on("-c", "--concurrency WEB_CONCURRENCY", 'Default is based on dyno size') { |v| options[:web_concurrency] = v}
 end.parse!
 
@@ -45,10 +45,10 @@ end
 heroku = PlatformAPI.connect_oauth(heroku_api_token)
 
 default_config = {
-  '1X' => {
+  'standard-1x' => {
     'WEB_CONCURRENCY' => '2',
   },
-  '2X' => {
+  'standard-2x' => {
     'WEB_CONCURRENCY'                    => '4',
     'RUBY_GC_HEAP_GROWTH_MAX_SLOTS'      => '400000',
     'RUBY_GC_HEAP_GROWTH_FACTOR'         => '1.1',
@@ -58,7 +58,7 @@ default_config = {
     'RUBY_GC_OLDMALLOC_LIMIT'            => '16000000',
     'RUBY_GC_OLDMALLOC_LIMIT_MAX'        => '16000000',
   },
-  'PX' => {
+  'performance-l' => {
     'WEB_CONCURRENCY' => '15',
     'RUBY_GC_HEAP_GROWTH_MAX_SLOTS'      => nil,
     'RUBY_GC_HEAP_GROWTH_FACTOR'         => nil,
@@ -94,11 +94,16 @@ if size && size != current_formation["size"]
   end
 
   # setting values to nil will unset the config value
+  if default_config[size].nil?
+    puts "#{size} was not found in the default configuration settings. Please use one of: #{default_config.keys}."
+    exit
+  end
+
   config.update_all(default_config[size])
 
   # The direction determines the order of the commands
   # If we are scaling UP, update formation before updating config
-  if size == 'PX' || (size == '2X' && current_formation['size'] == '1X')
+  if size == 'performance-l' || (size == 'standard-2X' && current_formation['size'] == 'Standard-1X')
     puts "Switching command order to formation, then config}"
     commands = [:formation, :config]
   end
@@ -107,9 +112,9 @@ else
 end
 
 # if preboot is enabled and we're running more than one dyno,
-# we have to sleep 4 minutes between commands, otherwise we get H99 errors
+# we have to sleep 3 minutes between commands, otherwise we get H99 errors
 if preboot_enabled && (current_formation["quantity"].to_i != 1 && options[:quantity].to_i != 1)
-  sleep_duration = 4 * 60
+  sleep_duration = 3 * 60
 else
   sleep_duration = 0
 end
